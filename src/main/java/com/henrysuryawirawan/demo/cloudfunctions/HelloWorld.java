49cdafd54f9c57f85c659ae85d5868c4c9198064
Henry Suryawirawan <hsuryawirawan@google.com>
2019-05-21 21:21:48 +0800

    Add HelloWorld

---
+++ src/main/java/com/henrysuryawirawan/demo/cloudfunctions/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.henrysuryawirawan.demo.cloudfunctions;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorld {
+
+  public void helloWorld(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+
+    String name = request.getParameter("name");
+    if (name == null) {
+      name = "World";
+    }
+
+    PrintWriter writer = response.getWriter();
+    writer.write("Hello " + name + "!");
+  }
+
+}
